c7afa00b326c4b7210c9bcfb9bcab953be3d4d81
Glowman554 <vossjanick62@gmail.com>
2026-01-08 14:52:49 +0100

    update microos

--- microos/build.sh
+++ microos/build.sh
@@ -1,6 +1,6 @@
-wget https://files.catbox.moe/tcfwq4.elf -O cdrom/mckrnl.elf
-wget https://files.catbox.moe/k5jtd7.syms -O cdrom/mckrnl.syms
-wget https://files.catbox.moe/dzf8u5.saf -O cdrom/initrd.saf
+wget https://files.catbox.moe/d5ff4t.elf -O cdrom/mckrnl.elf
+wget https://files.catbox.moe/vfolyo.syms -O cdrom/mckrnl.syms
+wget https://files.catbox.moe/vqt8u1.saf -O cdrom/initrd.saf
 
 xorriso -as mkisofs -R -r -J -b boot/limine/limine-cd.bin \
 	-no-emul-boot -boot-load-size 4 -boot-info-table -hfsplus \
